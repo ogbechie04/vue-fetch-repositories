587484e76529353133fcf838e5f2ceb20201688e
Laura Ogbechie <ogbechielaura04@gmail.com>
2024-06-01 17:22:20 +0100

    Completed functionality and styling for repos home

--- src/components/RepoHome.vue
+++ src/components/RepoHome.vue
@@ -14,20 +14,46 @@
         <p>Stars: {{ repo.stargazers_count }}</p>
       </div>
     </div>
-    <div class="textErros flex justify-center gap-3">
-      <button @click="triggerErrorTest">Trigger Error</button>
+    <div class="testErros flex justify-center gap-4 font-medium my-5">
+      <button
+        @click="triggerErrorTest"
+        class="px-3 py-1 rounded border-[1px] hover:-translate-y-1 transition-all border-solid border-blue-500 hover:bg-blue-500 hover:text-white"
+      >
+        Trigger Error
+      </button>
       <router-link to="/thispagedoesnotexist">
-        <button>Test 404 Page</button>
+        <button
+          class="px-3 py-1 rounded border-[1px] hover:-translate-y-1 transition-all border-solid border-blue-500 hover:bg-blue-500 hover:text-white"
+        >
+          Test 404 Page
+        </button>
       </router-link>
     </div>
-    <div class="pagination flex justify-center gap-3 my-5">
-      <button @click="handleChangePage(currentPage - 1)" :disabled="currentPage === 1">
+    <div class="pagination flex justify-center gap-3 my-7">
+      <button
+        class="bg-gray-200 px-3 py-1 rounded cursor-pointer"
+        @click="handleChangePage(currentPage - 1)"
+        :disabled="currentPage === 1"
+      >
         Previous
       </button>
-      <button v-for="page in totalPages" :key="page" @click="handleChangePage(page)">
+      <button
+        :class="{
+          'bg-blue-500 text-white': page === currentPage,
+          'bg-gray-200': page !== currentPage
+        }"
+        class="px-3 py-1 rounded cursor-pointer"
+        v-for="page in totalPages"
+        :key="page"
+        @click="handleChangePage(page)"
+      >
         {{ page }}
       </button>
-      <button @click="handleChangePage(currentPage + 1)" :disabled="currentPage === totalPages">
+      <button
+        class="bg-gray-200 px-3 py-1 rounded cursor-pointer"
+        @click="handleChangePage(currentPage + 1)"
+        :disabled="currentPage === totalPages"
+      >
         Next
       </button>
     </div>
@@ -102,3 +128,51 @@ export default {
   }
 }
 </script>
+
+<!-- HTML !-->
+<!-- <button class="button-28" role="button">Button 28</button> -->
+
+<!-- /* CSS */
+.button-28 {
+  appearance: none;
+  background-color: transparent;
+  border: 2px solid #1A1A1A;
+  border-radius: 15px;
+  box-sizing: border-box;
+  color: #3B3B3B;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  font-size: 16px;
+  font-weight: 600;
+  line-height: normal;
+  margin: 0;
+  min-height: 60px;
+  min-width: 0;
+  outline: none;
+  padding: 16px 24px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 100%;
+  will-change: transform;
+}
+
+.button-28:disabled {
+  pointer-events: none;
+}
+
+.button-28:hover {
+  color: #fff;
+  background-color: #1A1A1A;
+  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+  transform: translateY(-2px);
+}
+
+.button-28:active {
+  box-shadow: none;
+  transform: translateY(0);
+} -->
